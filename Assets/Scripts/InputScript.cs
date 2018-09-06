f39efba15af0af277bdcb31497a0d9ad2b286d89
DerpScarab <scarabbeetlearmy@gmail.com>
2018-09-06 09:47:52 -0700

    added input detection

---
+++ Assets/Scripts/InputScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputScript : MonoBehaviour {
+
+    public int controller_x = 0;
+    public int controller_y = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            controller_x = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            controller_x = -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            controller_y = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            controller_y = -1;
+        }
+    }
+}
---
+++ Assets/Scripts/MovesScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovesScript : MonoBehaviour {
+
+    public InputScript callInputScript = new InputScript();
+    
+	// Update is called once per frame
+	void Update () {
+		if (callInputScript.controller_x == 1)
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+
+        if (callInputScript.controller_x == -1)
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+
+        if (callInputScript.controller_y == 1)
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+
+        if (callInputScript.controller_y == -1)
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+    }
+}
